cbd6323472a378a7e61442cbdb59543e1dd96df7
Swapnil Bhosale <bswapnil699@gmail.com>
2024-04-13 18:03:09 +0530

    add a for calling 3api

--- src/main/java/com/ecommerce/productservice/service/ProductService.java
+++ src/main/java/com/ecommerce/productservice/service/ProductService.java
@@ -1,9 +1,25 @@
 package com.ecommerce.productservice.service;
 
+import com.ecommerce.productservice.dtos.GetProductDto;
+import com.ecommerce.productservice.models.Product;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ProductService {
+    public GetProductDto getProductDto(Long id){
 
+        RestTemplate restTemplate =new RestTemplate();
 
+        String Url = "https://fakestoreapi.com/products/" + id ;
+
+        Product product = restTemplate.getForObject(Url, Product.class);
+        System.out.println(product);
+
+        GetProductDto obj = new GetProductDto();
+        obj.setName(product.getTitle());
+        obj.setPrice(product.getPrice());
+        obj.setImageUrl(product.getImage());
+        return obj;
+    }
 }
